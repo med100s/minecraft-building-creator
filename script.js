93affe23f1e04d86e967918b98efd24fe244c381
nikita <61676013+med100s@users.noreply.github.com>
2022-07-19 09:57:19 +0500

    added some blocks

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let object = {}
 
 
+
 let num = 0
 // console.log(object["1"])
 
@@ -34,39 +35,59 @@ let last_command_part = `
 let brackets_command_part = ``
 
 const materials = {
-    "Stone Bricks":"stonebrick",
-    "Clay":"clay",
-    "Torch":"torch",
-    "Glass Pane":"glass"
+    "Stone Bricks": "stonebrick",
+    "Clay": "clay",
+    "Torch": "torch",
+    "Cobblestone": "cobblestone",
+    "Glass Pane": "glass",
+    "Green Wool": "wool",
+    "Netherrack": "netherrack",
+    "Spruce Wood": "log",
+    "Cobblestone Slab": "stone_slab",
+    "Oak Wood Plank": "planks",
+    "Fire": "lava",
+    "Furnace": "furnace",
+    "Jukebox": "noteblock",
+    "Lever": "lever",
+    "Dispenser": "dispenser",
+    "Oak Fence": "dark_oak_fence",
+    "Wooden Pressure Plate": "Wooden_pressure_plate",
+    "Bed": "bed",
+    "Chest": "chest",
+    "Oak Door": "dark_oak_door",
+    "Wooden Trapdoor": "trapdoor",
+    "Ladder": "ladder",
+    "Oak Wood Stairs": "spruce_stairs"
+
 }
 
-let layer = 1 
-let layer_plus  = layer + 1
+let layer = 1
+let layer_plus = layer + 1
 let level = 61
 // for(let layer in object){
-for ( ; layer < layer_plus ; layer++) {
-    for (let prop in object[layer]) {
+for (; layer <= Object.keys(object).length; layer++) { 
+        for (let prop in object[layer]) {
 
-        for (let prop2 in object[layer][prop]) {
+            for (let prop2 in object[layer][prop]) {
 
-            command += `
+                command += `
             [{id:falling_block,Block:command_block,Time:1,TileEntityData:
-            {Command:"/setblock ~${object[layer][prop][prop2]["x"]} ${level + layer} ~${object[layer][prop][prop2]["z"]} ${materials[object[layer][prop][prop2]["name"]]?materials[object[layer][prop][prop2]["name"]]:'grass'}"},Passengers:     
+            {Command:"/setblock ~${object[layer][prop][prop2]["x"]} ${level + layer} ~${object[layer][prop][prop2]["z"]} ${materials[object[layer][prop][prop2]["name"]] ? materials[object[layer][prop][prop2]["name"]] : 'grass'}"},Passengers:     
             `
 
-            console.log(object[layer][prop][prop2]["x"], object[layer][prop][prop2]["z"], layer)
-            brackets_command_part += `}]`
-            num += 1
-        }
-    } 
+                console.log(object[layer][prop][prop2]["x"], object[layer][prop][prop2]["z"], layer)
+                brackets_command_part += `}]`
+                num += 1
+            } 
+    }
+    // }
+
+    let full_command = (first_command_part + command + last_command_part + brackets_command_part + "}")
+    // console.log(first_command_part, command, last_command_part, brackets_command_part, "}")
+
+    fs = require('fs');
+    fs.writeFile(`helloworld${layer}.txt`, full_command, function (err) {
+        if (err) return console.log(err);
+        console.log('Hello World > helloworld.txt');
+    });
 }
-// }
-
-let full_command = (first_command_part + command + last_command_part + brackets_command_part + "}")
-// console.log(first_command_part, command, last_command_part, brackets_command_part, "}")
-
-fs = require('fs');
-fs.writeFile('helloworld.txt', full_command, function (err) {
-    if (err) return console.log(err);
-    console.log('Hello World > helloworld.txt');
-});
